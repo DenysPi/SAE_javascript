9ac7e1cea352526519b5ab804ad618d7b1f6a6ed
Denys Piddubnyi <denispiddubnij9@gmail.com>
2026-05-25 14:02:32 +0200

    affichage de gameArea

--- js/DOMManager.js
+++ js/DOMManager.js
@@ -1,20 +1,25 @@
 export class DOMManager {
 
-
+  #selectors = {
+    gameArea: '.game-area',
+    gameBoard: '.game-board',
+    gameForm: '.game-form',
+  }
   /**
    * Ajoute toutes les images d'une collection sur le gameBoard
    * @param {Image[]} images
    */
   createCards(images) {
-    const gameBoard = document.querySelector('.game-board');
+    const gameBoard = document.querySelector(this.#selectors.gameBoard);
 
     gameBoard.innerHTML = ''; 
     // Todo À Compléter
-    images.forEach(image => {
+    images.forEach((image, index) => {
       const card = document.createElement('div');
       card.classList.add('card');
 
       card.dataset.imageId = image.id;
+      card.dataset.index = index;
       
       card.innerHTML = `
         <div class="card-inner">
@@ -28,5 +33,10 @@ export class DOMManager {
       gameBoard.append(card);
     });
     
+  }
+
+  afficherGameArea() {
+    document.querySelector(this.#selectors.gameForm).classList.add('hidden');
+    document.querySelector(this.#selectors.gameArea).classList.remove('hidden');
   }
 }
